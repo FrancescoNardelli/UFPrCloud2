facfa7c6750ae7bb90eb58cf6d90014706ffb414
FrancescoNardelli <francesco.nardelli@outlook.it>
2018-09-24 10:17:34 +0200

    Primo commit - Esercizio 2

---
+++ shop-recommender/src/main/java/com/example/shoprecommender/rest/RecommenderController.java
@@ -0,0 +1,23 @@
+package com.example.shoprecommender.rest;
+
+import java.awt.print.Pageable;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.shoprecommender.service.RecommenderService;
+
+@RestController
+public class RecommenderController {
+	
+	@Autowired
+	private RecommenderService service;
+	
+	@GetMapping("/api/recommendations/{productId}/{userId}")
+	public @ResponseBody Page<Product> listRecommended(Pageable pageRequest,@PathVariable String productId, @PathVariable String userId) {
+		return null;
+	}
+}
---
+++ shop-recommender/src/main/java/com/example/shoprecommender/service/RecommenderService.java
@@ -0,0 +1,11 @@
+package com.example.shoprecommender.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class RecommenderService {
+
+	public Page<Product> getPurchasesFromUserID(String userId) {
+		return null;
+	}
+}
